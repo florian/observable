464240fd5b1c07f65250a334cf0c1fc724eab978
Florian H <jscoderr@gmail.com>
2012-12-20 22:42:47 +0100

    test if on, off and trigger are functions

--- spec/spec.coffee
+++ spec/spec.coffee
@@ -1,3 +1,17 @@
+class A extends Observable
+
 describe 'Observable', ->
 	it 'should be a property of window', ->
 		expect(window).to.have.property('Observable')
+
+	describe 'on', ->
+		it 'should be a function', ->
+			expect(A.on).to.be.a('function')
+
+	describe 'off', ->
+		it 'should be a function', ->
+			expect(A.off).to.be.a('function')
+
+	describe 'trigger', ->
+		it 'should be a function', ->
+			expect(A.trigger).to.be.a('function')
--- spec/spec.js
+++ spec/spec.js
@@ -1,8 +1,38 @@
 (function() {
+  var A,
+    __hasProp = {}.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+
+  A = (function(_super) {
+
+    __extends(A, _super);
+
+    function A() {
+      return A.__super__.constructor.apply(this, arguments);
+    }
+
+    return A;
+
+  })(Observable);
 
   describe('Observable', function() {
-    return it('should be a property of window', function() {
+    it('should be a property of window', function() {
       return expect(window).to.have.property('Observable');
+    });
+    describe('on', function() {
+      return it('should be a function', function() {
+        return expect(A.on).to.be.a('function');
+      });
+    });
+    describe('off', function() {
+      return it('should be a function', function() {
+        return expect(A.off).to.be.a('function');
+      });
+    });
+    return describe('trigger', function() {
+      return it('should be a function', function() {
+        return expect(A.trigger).to.be.a('function');
+      });
     });
   });
 
