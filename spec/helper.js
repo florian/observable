bf274580f4f43edaa822ee79bccd4e19eceed06b
Florian H <jscoderr@gmail.com>
2012-12-20 21:58:11 +0100

    add /testem.js if tests are run by testem

--- spec/helper.js
+++ spec/helper.js
@@ -1,7 +1,13 @@
+// testem
+if (location.href.indexOf('http://localhost:7357') == 0) {
+    document.write('<script src="/testem.js"><\/script>');
+}
+
 var expect = chai.expect;
 
 mocha.setup({
-    ui: 'bdd'
+    ui: 'bdd',
+    ignoreLeaks: true
 });
 
 $(function () {
